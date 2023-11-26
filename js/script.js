4b27c522b8e8f5847972d38010c39ff3e7a0e4f0
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-11-26 20:44:03 -0300

    :construction: feat:  Update CSS and JS code for responsiveness

--- js/script.js
+++ js/script.js
@@ -9,5 +9,21 @@ var swiper = new Swiper(".slide-video", {
 	navigation: {
 		prevEl: ".s-videos .btn-prev",
 		nextEl: ".s-videos .btn-next"
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1.1,
+			spaceBetween: 16
+		},
+		767: {
+			slidesPerView: 2.1
+		},
+		1024: {
+			slidesPerView: 3.1
+		},
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 30
+		}
 	}
 });
